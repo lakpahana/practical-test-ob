20398c3c0601a4a9cc6c6d28416862c880284754
AGS Lakpahana <agslakpahana@gmail.com>
2024-05-09 17:35:36 +0530

    refactor

--- api/views.py
+++ api/views.py
@@ -1,62 +1,27 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
-from .models import School, AssessmentAreas, Awards, Class, Student, Subject, Answers, Summary,CorrectAnswers
+from .models import School, AssessmentAreas, Awards, Class, Student, Subject, Answers, Summary, CorrectAnswers
 
-# views.py
-def index(request):
-    # Get all objects
-    schools = School.objects.all()
-    assessment_areas = AssessmentAreas.objects.all()
-    awards = Awards.objects.all()
-    classes = Class.objects.all()
-    students = Student.objects.all()
-    subjects = Subject.objects.all()
-    answers = Answers.objects.all()
-    summaries = Summary.objects.all()
-    correct_answers = CorrectAnswers.objects.all()
+def paginate_query(request, query_set, count, page_query_param):
+    paginator = Paginator(query_set, count)  
+    page_number = request.GET.get(page_query_param)  
+    return paginator.get_page(page_number)  
 
-    # Create a Paginator object for each model
-    schools_paginator = Paginator(schools, 10)  # Show 10 schools per page
-    assessment_areas_paginator = Paginator(assessment_areas, 10)  # Show 10 assessment areas per page
-    awards_paginator = Paginator(awards, 10)  # Show 10 awards per page
-    classes_paginator = Paginator(classes, 10)  # Show 10 classes per page
-    students_paginator = Paginator(students, 10)  # Show 10 students per page
-    subjects_paginator = Paginator(subjects, 10)  # Show 10 subjects per page
-    answers_paginator = Paginator(answers, 10)  # Show 10 answers per page
-    summaries_paginator = Paginator(summaries, 10)  # Show 10 summaries per page
-    correct_answers_paginator = Paginator(correct_answers, 10)  # Show 10 correct answers per page
-
-    # Get the page number from the query string for each paginator
-    schools_page_number = request.GET.get('schools_page')
-    assessment_areas_page_number = request.GET.get('assessment_areas_page')
-    awards_page_number = request.GET.get('awards_page')
-    classes_page_number = request.GET.get('classes_page')
-    students_page_number = request.GET.get('students_page')
-    subjects_page_number = request.GET.get('subjects_page')
-    answers_page_number = request.GET.get('answers_page')
-    summaries_page_number = request.GET.get('summaries_page')
-    correct_answers_page_number = request.GET.get('correct_answers_page')
-
-    # Get the Page object for this page for each paginator
-    schools_page = schools_paginator.get_page(schools_page_number)
-    assessment_areas_page = assessment_areas_paginator.get_page(assessment_areas_page_number)
-    awards_page = awards_paginator.get_page(awards_page_number)
-    classes_page = classes_paginator.get_page(classes_page_number)
-    students_page = students_paginator.get_page(students_page_number)
-    subjects_page = subjects_paginator.get_page(subjects_page_number)
-    answers_page = answers_paginator.get_page(answers_page_number)
-    summaries_page = summaries_paginator.get_page(summaries_page_number)
-    correct_answers_page = correct_answers_paginator.get_page(correct_answers_page_number)
-    context = {
-        'schools': schools_page,
-        'assessment_areas': assessment_areas_page,
-        'awards': awards_page,
-        'classes': classes_page,
-        'students': students_page,
-        'subjects': subjects_page,
-        'answers': answers_page,
-        'summaries': summaries_page,
-        'correct_answers': correct_answers_page,
+def index(request):    
+    model_page_dict = {
+        'schools': (School.objects.all(), 'schools_page', 10),
+        'assessment_areas': (AssessmentAreas.objects.all(), 'assessment_areas_page', 10),
+        'awards': (Awards.objects.all(), 'awards_page', 10),
+        'classes': (Class.objects.all(), 'classes_page', 10),
+        'students': (Student.objects.all(), 'students_page', 10),
+        'subjects': (Subject.objects.all(), 'subjects_page', 10),
+        'answers': (Answers.objects.all(), 'answers_page', 10),
+        'summaries': (Summary.objects.all(), 'summaries_page', 10),
+        'correct_answers': (CorrectAnswers.objects.all(), 'correct_answers_page', 10)
     }
+
+    context = {}
+    for key, value in model_page_dict.items():
+        context[key] = paginate_query(request, value[0], value[2], value[1])  
 
     return render(request, 'index.html', context)
